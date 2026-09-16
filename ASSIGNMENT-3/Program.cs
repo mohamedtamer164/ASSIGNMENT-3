a1e9fffbdea90cf46f29242dd08fd0e88b450951
original <original@DESKTOP-H4JMMMN>
2026-09-16 10:46:52 +0300

    Answer1

--- ASSIGNMENT-3/Program.cs
+++ ASSIGNMENT-3/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // answer 1 
+            #region array 1d
+            //   double[] prices =new double[] { 22.5, 40.32, 33, 75 };  ممكن تتكتب كدا بس مش محببه 
+            double[] prices = { 22.5, 40.32, 33, 75 };
+            Console.WriteLine(prices[1]);
+            //  الكود دا حبيت ان المستخدم هو الي يدخل الارقام و يعمل الloop  المناسب للطباعه 
+            double[] prices2 = new double[3];
+            for(int i = 0; i < prices2.Length; i++)
+            {
+                Console.Write("enter number of array :");
+                prices2[i] = double.Parse(Console.ReadLine());
+               
+
+
+            }
+            Console.WriteLine(prices2[1]);
+            #endregion
         }
     }
 }
